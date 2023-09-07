e056a4036d674b8c75fd22bcf0d7fecef4fd0c1c
Leandro Thiago Ribeiro <111009157+LeandroTRibeiro@users.noreply.github.com>
2023-09-07 18:25:58 -0300

    add ex 19, 20

--- EstruturaSequencial/Exercises/ex19.c
+++ EstruturaSequencial/Exercises/ex19.c
@@ -7,6 +7,63 @@ freqüência e média mínima para aprovação.
 
 #include <stdio.h>
 
-main() {
+struct Dicipline {
+    char name[100];
+    char description[100];
+    float frequence;
+    float grade;
+};
+
+int main() {
+    int length=0;
+    char plusLength;
+
+    struct Dicipline diciplines[100];
+
+    printf("\n-----------------------------------\n"); 
+    printf("-----------------------------------\n");
+    printf("CADASTRO DE DISCIPLINAS");
+    printf("\n-----------------------------------\n"); 
+    printf("-----------------------------------\n");
+
+    do {
+        printf("\ndigite o nome da disciplina: ");
+        scanf(" %[^\n]", diciplines[length].name);
+
+        printf("digite a descricao: ");
+        scanf(" %[^\n]", diciplines[length].description);
+
+        printf("digite a frequencia minima: ");
+        scanf("%f", &diciplines[length].frequence);
+
+        printf("digite a media: ");
+        scanf("%f", &diciplines[length].grade);
+
+        length++;
+
+        printf("\ngostaria de adicionar mais uma disciplina? (S/N): ");
+        scanf(" %c", &plusLength);
+
+        printf("\n-----------------------------------\n"); 
+        printf("-----------------------------------\n");
+
+    } while (plusLength == 'S' || plusLength == 's');
+
+    printf("\n-----------------------------------\n"); 
+    printf("-----------------------------------\n");
+    
+    for (int i = 0; i < length; i++) {
+        printf("\nCadastro: %d", i+1);
+        printf("\ndisciplina: %s", diciplines[i].name);
+        printf("\ndescricao: %s", diciplines[i].description);
+        printf("\nfrequencia: %.2f%%", diciplines[i].frequence);
+        printf("\nmedia: %.2f", diciplines[i].grade);
+        printf("\n-----------------------------------\n"); 
+        printf("-----------------------------------\n");
+    }
+
+    printf("\nFim do programa.\n");
+
+    return 0;
     
 }
---
+++ EstruturaSequencial/Exercises/ex20.c
@@ -0,0 +1,7 @@
+/*
+
+20. Elabore um algoritmo que leia dois valores para as variáveis A e B, efetue a troca dos valores de forma 
+que a variável A passe a ter o valor da variável B, e que a variável B passe a ter o valor da variável A. 
+Ao final, imprima os valores com a troca efetuada.
+
+*/
